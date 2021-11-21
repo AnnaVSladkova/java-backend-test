8b28a93b054316698af5c410f8e4beae25fa1b0d
Anna Sladkova <sladkovaav@gmail.com>
2021-11-21 22:48:17 +0300

    lesson7

---
+++ src/test/java/lesson7/ProductTest.java
@@ -0,0 +1,56 @@
+package lesson7;
+
+import com.github.javafaker.Faker;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.geekbrains.javaback.db.dao.ProductsMapper;
+import ru.geekbrains.javaback.dto.Product;
+import ru.geekbrains.javaback.enums.CategoryType;
+import ru.geekbrains.javaback.utils.DbUtils;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ProductTest {
+
+    static ProductsMapper productsMapper;
+    int productId;
+
+    Faker faker = new Faker();
+    Product product;
+
+    @BeforeAll
+    static void beforeAll() {
+        productsMapper = DbUtils.getProductsMapper();
+    }
+
+    @BeforeEach
+    void setUp() {
+        product = new Product()
+                .withTitle(faker.food().dish())
+                .withPrice((int) (Math.random() + 1) * 100)
+                .withCategoryId(CategoryType.FOOD.getId())
+                .withCategoryTitle(CategoryType.FOOD.getTitle());
+    }
+
+    @Test
+    void postProductTest() throws IOException {
+        Integer countProductsBefore = DbUtils.countProducts(productsMapper);
+        product = DbUtils.createProducts(productsMapper, product);
+        Integer countProductsAfter = DbUtils.countProducts(productsMapper);
+        assertThat(countProductsAfter, equalTo(countProductsBefore+1));
+        productId = product.getId();
+    }
+
+    @SneakyThrows
+    @AfterEach
+    void tearDown() {
+        DbUtils.deleteProduct(productsMapper, productId);
+    }
+
+}
---
+++ src/test/java/lesson7/UpdateProductTest.java
@@ -0,0 +1,55 @@
+package lesson7;
+
+import com.github.javafaker.Faker;
+import lombok.SneakyThrows;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.geekbrains.javaback.db.dao.ProductsMapper;
+import ru.geekbrains.javaback.dto.Product;
+import ru.geekbrains.javaback.enums.CategoryType;
+import ru.geekbrains.javaback.utils.DbUtils;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class UpdateProductTest {
+    static ProductsMapper productsMapper;
+    Faker faker = new Faker();
+    Product product;
+
+    @BeforeAll
+    static void beforeAll() {
+        productsMapper = DbUtils.getProductsMapper();
+    }
+    @SneakyThrows
+    @BeforeEach
+    void setUp() {
+        product = new Product()
+                .withTitle(faker.food().dish())
+                .withPrice(100)
+                .withCategoryTitle(CategoryType.FOOD.getTitle())
+                .withCategoryId(CategoryType.FOOD.getId());
+        product = DbUtils.createProducts(productsMapper, product);
+    }
+
+
+    @SneakyThrows
+    @Test
+    void putProductTest() throws IOException{
+        product.setPrice(200);
+        DbUtils.updateProduct(productsMapper, product);
+        Product _product = DbUtils.getProduct(productsMapper, product.getId());
+        assertThat(_product.getPrice(), CoreMatchers.is(product.getPrice()));
+    }
+
+    @SneakyThrows
+    @AfterEach
+    void tearDown() {
+        DbUtils.deleteProduct(productsMapper, product.getId());
+    }
+
+}
